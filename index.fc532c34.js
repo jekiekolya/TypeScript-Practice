bf47d3e74d05efbf974e99b9a82243cd0be1d975
jekiekolya <108493349+jekiekolya@users.noreply.github.com>
2022-12-31 17:27:28 +0000

    Deploying to gh-pages from @ jekiekolya/TypeScript-Practice@4baebef1e2a7d142bfc6f4a37611a75f4063a2fe 🚀

--- index.fc532c34.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,o,a={};a.Nick=23,a.Jack=25,(o=t||(t={}))[o.North=1]="North",o.East="east",o[o.South=200]="South",o[o.West=201]="West"}();
-//# sourceMappingURL=index.fc532c34.js.map
---
+++ index.fdceca43.js
@@ -0,0 +1,2 @@
+let t="string";t=5;let s="string",r=s;s=4,r=s,s=5;let e=[1,"string",!0];e=[null,"string",void 0];const o={};let l;var n;o.Nick=23,o.Jack=25,(n=l||(l={}))[n.North=1]="North",n.East="east",n[n.South=200]="South",n[n.West=201]="West";var a;a="true",console.log("args:",a);
+//# sourceMappingURL=index.fdceca43.js.map
