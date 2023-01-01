ecb9195cc8894bad6669dcd833b812ab47340145
jekiekolya <108493349+jekiekolya@users.noreply.github.com>
2023-01-01 13:18:10 +0000

    Deploying to gh-pages from @ jekiekolya/TypeScript-Practice@3bc663fd5e937320f59993caaf840b7fb540d5c3 🚀

--- index.9f57e7b8.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,o,a={};a.Nick=23,a.Jack=25,(o=t||(t={}))[o.North=1]="North",o.East="east",o[o.South=200]="South",o[o.West=201]="West";var r;r="true",console.log("args:",r)}();
-//# sourceMappingURL=index.9f57e7b8.js.map
