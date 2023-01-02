35fc81c12f8f141d1e89d2e5afcf83addef38332
jekiekolya <108493349+jekiekolya@users.noreply.github.com>
2023-01-02 15:07:50 +0000

    Deploying to gh-pages from @ jekiekolya/TypeScript-Practice@46178ff1328191055a92fbbfacc2026d87f3a2d8 🚀

---
+++ index.98be6613.js
@@ -0,0 +1,2 @@
+!function(){function e(e){return e&&e.__esModule?e.default:e}var n={};function o(e,n){console.log("Printing person property ".concat(n,': "').concat(e[n],'"'))}Object.defineProperty(n,"__esModule",{value:!0}),n.default=function(e,n,o){n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o;return e};var r,t,a={name:"Jon",age:23};o(a,"name"),o(a,"age"),console.log((r="code",t="34234",e(n)({},r,t)))}();
+//# sourceMappingURL=index.98be6613.js.map
---
+++ index.d450bd01.js
@@ -0,0 +1,2 @@
+function o(o,e){console.log(`Printing person property ${e}: "${o[e]}"`)}let e={name:"Jon",age:23};o(e,"name"),o(e,"age"),console.log({["code"]:"34234"});
+//# sourceMappingURL=index.d450bd01.js.map
